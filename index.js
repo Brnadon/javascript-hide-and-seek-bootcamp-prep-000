d696c26d4acde0d391f5d61664e2b5b4f912bb4a
Brandon Brasson <rocksong622@gmail.com>
2017-09-27 21:47:39 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,36 @@
+
+function nestedTarget(){
+  let element = document.querySelectorAll('#nested.target')
+    element = document.querySelector('div')
+  return element
+}
+
+
+querySelector() 
+function getFirstSelector(selector){
+ let element = document.querySelector(`${selector}`)
+   return element
+ }
+function querySelectorAll() {
+  
+} 
+
+function div#grand-node () {
+  
+}
+
+
+
+
+
+ function increaseRankBy(n){
+   let rank = document.querySelectorAll('.ranked-list li')
+   for (var i = 0; i < rank.length; i++){
+     return parseInt(rank[i].innerHTML) + n
+   }
+ }
+ 
+function deepestChild(){
+  let child = document.querySelector('div#grand-node div div div div')
+    return child
+ }
